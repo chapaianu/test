5e3a7c8fffc10d96ebf730aeb577f4ad086e6a28
chapaianu <constantin.hapaianu@gmail.com>
2020-11-10 17:01:39 +0200

    Update LocaleTest.java

--- LocaleTest.java
+++ LocaleTest.java
@@ -1,7 +1,50 @@
+
 import java.util.Locale;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Locale;
+import java.util.Scanner;
+
 
 public class LocaleTest {
+
 	public static final void main(String[] args) {
 		System.out.println("Locale: " + Locale.US.toString());
+		
+		String filePath = "/data/content/player/emails/en_US/accountCreateWithValidation.txt";
+		try {
+			String fileContent = readFileAsString(filePath);
+			System.out.println("FileContent:\n" + fileContent);
+		} catch (Exception ex) {
+			System.out.println("Exception: " + ex.printStackTrace());
+		}
 	}
+	
+	private String readFileAsString(String filePath) throws java.io.IOException {
+
+		byte[] buffer = new byte[(int) new File(filePath).length()];
+		BufferedInputStream f = null;
+		String scannerFinal = "";
+		try {
+			f = new BufferedInputStream(new FileInputStream(filePath));
+			f.read(buffer);
+
+			String scanner = new Scanner(new File(filePath)).useDelimiter("\\Z").next();
+			scannerFinal = scanner;
+		} finally {
+			if (f != null)
+				try {
+					f.close();
+				} catch (IOException ignored) {
+				}
+		}
+
+		ByteBuffer bb = ByteBuffer.allocate(buffer.length);
+
+		return scannerFinal;
+	}
+
 }
